8f00492fcebcea2b213409cd7d9e65717e045ca8
mawerb <maorbarzilay8@gmail.com>
2025-08-14 04:39:25 -0700

    revert

--- server/csulbroutes/settings.py
+++ server/csulbroutes/settings.py
@@ -117,7 +117,6 @@ WSGI_APPLICATION = 'csulbroutes.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL'),
-        NAME="eventsdb",
         conn_max_age=600,
     )
 }
