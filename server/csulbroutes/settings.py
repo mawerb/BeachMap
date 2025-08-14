06422c3b322c25acb9c58f9508670ab0053b98ae
mawerb <maorbarzilay8@gmail.com>
2025-08-14 00:25:03 -0700

    security fix

--- server/csulbroutes/settings.py
+++ server/csulbroutes/settings.py
@@ -30,12 +30,12 @@ CORS_ALLOWED_ORIGINS = [
     ]
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-km=r4h4izlhq5_2)hn%cndqfnqe8!^od_l)4xppn8wo0@x@2ck'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['csulbroutes-delicate-night-320.fly.dev']
 
 
 # Application definition
